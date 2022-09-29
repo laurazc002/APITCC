b8d70bae6a1c1e68e2ad38fa5d65fadd6cd121cf
Darka304 <esteban.1123@hotmail.com>
2022-09-29 12:06:29 -0500

    se crea api rest en spring boot

---
+++ src/main/java/com/tcc/apitcc/servicios/MercanciaServicio.java
@@ -0,0 +1,15 @@
+package com.tcc.apitcc.servicios;
+
+public interface MercanciaServicio {
+
+    //Consultar Mercancias
+
+
+    //Consultar Mercancia por IUP
+
+
+    //Guardar Mercancia
+
+    //Eliminar Mercancia
+
+}
